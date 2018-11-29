c22e07654a599bd894fe964255cf7462d51f230a
bradteeman44 <40871502+bradteeman44@users.noreply.github.com>
2018-11-29 16:18:24 -0500

    Adding the calendar selection

--- app/src/main/java/com/example/myapplication/AddChoreFragment.kt
+++ app/src/main/java/com/example/myapplication/AddChoreFragment.kt
@@ -1,13 +1,18 @@
 package com.example.myapplication
 
+import android.app.DatePickerDialog
+import android.icu.util.Calendar
+import android.os.Build
 import android.os.Bundle
+import android.support.annotation.RequiresApi
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.Spinner
-import androidx.navigation.findNavController
+import kotlinx.android.synthetic.main.add_chore_fragment.*
 import kotlinx.android.synthetic.main.add_chore_fragment.view.*
 import kotlinx.android.synthetic.main.initial_screen.view.*
 
@@ -21,11 +26,14 @@ class AddChoreFragment : Fragment() {
         }
     }
 
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
         var fragmentView = inflater.inflate(R.layout.add_chore_fragment, container, false)
         val spinner = fragmentView.assigned_to_spinner
+        val dateButton = fragmentView.assign_button
         setUpSpinner(spinner)
+        setUpDate(dateButton)
         return fragmentView
     }
 
@@ -39,5 +47,20 @@ class AddChoreFragment : Fragment() {
                           }
 
     }
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    private fun setUpDate(button: Button) {
+        val c = Calendar.getInstance()
+        val year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH)
+        val day = c.get(Calendar.DAY_OF_MONTH)
+        button.setOnClickListener {
+            val dpd = DatePickerDialog(this.context, DatePickerDialog.OnDateSetListener { view, mYear, mMonth, mDay ->
+                due_date_tv.setText("" + (mMonth + 1) + "/" + mDay + "/" + mYear)
+            }, year, month, day)
+            dpd.show()
+        }
+    }
+
 
 }
